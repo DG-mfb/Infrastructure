b2b0a154fc71309ec1648eff8854fb79c67bfad3
Daniel Georgiev <danail.georgiev73@gmail.com>
2017-07-24 23:41:22 +0100

    login page

--- AssetManagement/Client/Angular/src/app/app.module.ts
+++ AssetManagement/Client/Angular/src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { LoginComponent } from './login/login.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppComponent }  from './app.component';
 
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
-  bootstrap:    [ AppComponent ]
+	imports: [BrowserModule, FormsModule ],
+	declarations: [AppComponent, LoginComponent ],
+	bootstrap: [LoginComponent ]
 })
 export class AppModule { }
