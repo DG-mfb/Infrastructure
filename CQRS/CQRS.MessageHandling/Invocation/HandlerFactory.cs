7a4a5bc5f2d6c41d921e10e6a0b4c5ac659a68ce
Daniel Georgiev <danail.georgiev73@gmail.com>
2017-07-27 16:11:00 +0100

    CQRS unit test

---
+++ CQRS/CQRS.MessageHandling/Invocation/HandlerFactory.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using Kernel.Extensions;
+
+namespace CQRS.MessageHandling.Invocation
+{
+    internal class HandlerFactory
+    {
+        private static readonly ConcurrentDictionary<Tuple<Type, Type>, Func<object, object[], Task>> MessageHandlerDelegatesCache = new ConcurrentDictionary<Tuple<Type, Type>, Func<object, object[], Task>>();
+
+        public static Func<object, object[], Task> BuildMessageHandlerDelegate(Type handlerType, Type commandType)
+        {
+            return HandlerFactory.MessageHandlerDelegatesCache.GetOrAdd(new Tuple<Type, Type>(handlerType, commandType), t => HandlerFactory.BuildMessageHandlerDelegateInternal(handlerType, commandType));
+        }
+
+        private static Func<object, object[], Task> BuildMessageHandlerDelegateInternal(Type targetType, Type commandType)
+        {
+            return TypeExtensions.GetAsyncInvoker(targetType, "Handle", commandType);
+            //var method = targetType.GetMethods().Single(c => c.Name == "Handle");
+            //var targetParam = Expression.Parameter(typeof(object));
+            //var eventParam = Expression.Parameter(typeof(object));
+
+            //var handlerConvert = Expression.Convert(targetParam, targetType);
+            //var commandConvert = Expression.Convert(eventParam, commandype);
+
+            //var callExpression = Expression.Call(handlerConvert, method, commandConvert);
+            //var lambda = Expression.Lambda<Func<object, object, Task>>(callExpression, targetParam, eventParam);
+            //var compiledLambda = lambda.Compile();
+            //return compiledLambda;
+        }
+    }
+}
---
+++ Kernel/Kernel.CQRS/MessageHandling/CommandHandling/ICommandHandler.cs
@@ -0,0 +1,18 @@
+﻿namespace Kernel.CQRS.MessageHandling.CommandHandling
+{
+    using System.Threading.Tasks;
+    using Kernel.Initialisation;
+
+    /// <summary>
+    /// Provides methods to handle commands
+    /// </summary>
+    /// <typeparam name="TCommand">The type of the command.</typeparam>
+    public interface ICommandHandler<TCommand> : IAutoRegisterAsTransient where TCommand : Command.Command
+	{
+		/// <summary>
+		/// Handles the specified command.
+		/// </summary>
+		/// <param name="command">The command.</param>
+		Task Handle(TCommand command);
+	}
+}
