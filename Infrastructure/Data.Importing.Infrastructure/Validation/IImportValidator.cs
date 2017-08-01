73bf47d2d0769995e93e8247272556d145dbc286
dg-mfb <danail.georgiev73@gmail.com>
2017-08-01 22:33:39 +0100

    kernel refactoring

--- Infrastructure/Data.Importing.Infrastructure/Validation/IImportValidator.cs
+++ Infrastructure/Data.Importing.Infrastructure/Validation/IImportValidator.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Kernel.Validation;
+﻿using Kernel.Validation;
 
 namespace Data.Importing.Infrastructure.Validation
 {
