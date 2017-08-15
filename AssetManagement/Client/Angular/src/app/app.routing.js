7f0cfcdb80fe6613c044aef88a15ec119077d089
dg-mfb <danail.georgiev73@gmail.com>
2017-08-15 19:10:41 +0100

    adding EF useful stuff

--- AssetManagement/Client/Angular/src/app/app.routing.js
+++ AssetManagement/Client/Angular/src/app/app.routing.js
@@ -2,12 +2,12 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var login_component_1 = require("./login/login.component");
 exports.AppRoutes = [
-    { path: 'login', component: login_component_1.LoginComponent, data: { title: 'Login' } },
     {
         path: '',
         redirectTo: '/login',
         pathMatch: 'full'
     },
+    { path: 'login', component: login_component_1.LoginComponent, data: { title: 'Login' } },
     { path: '**', component: login_component_1.LoginComponent, data: { title: 'Login' } },
 ];
 //# sourceMappingURL=app.routing.js.map
