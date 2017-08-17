a98ae2fd72d5a22d3bd9b8f691130290c75cbb9d
dg-mfb <danail.georgiev73@gmail.com>
2017-08-17 19:29:38 +0100

    asp.net identity provider impl

---
+++ AuthenticationProviders/Microsoft.AspNet.Identity.Providers/AspNet.EntityFramework.IdentityProvider/Managers/ApplicationUserManager.cs
@@ -0,0 +1,38 @@
+﻿using AspNet.EntityFramework.IdentityProvider.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace AspNet.EntityFramework.IdentityProvider.Managers
+{
+    public class ApplicationUserManager : UserManager<ApplicationUser>
+    {
+        public ApplicationUserManager(IUserStore<ApplicationUser> store)
+            : base(store)
+        {
+        }
+
+        public static ApplicationUserManager Create(IUserTokenProvider<ApplicationUser, string> tokenProvider, ApplicationDbContext context)
+        {
+            var manager = new ApplicationUserManager(new UserStore<ApplicationUser>(context));
+            // Configure validation logic for usernames
+            manager.UserValidator = new UserValidator<ApplicationUser>(manager)
+            {
+                AllowOnlyAlphanumericUserNames = false,
+                RequireUniqueEmail = true
+            };
+            // Configure validation logic for passwords
+            manager.PasswordValidator = new PasswordValidator
+            {
+                RequiredLength = 6,
+                RequireNonLetterOrDigit = true,
+                RequireDigit = true,
+                RequireLowercase = true,
+                RequireUppercase = true,
+            };
+
+            manager.UserTokenProvider = tokenProvider;
+            
+            return manager;
+        }
+    }
+}
