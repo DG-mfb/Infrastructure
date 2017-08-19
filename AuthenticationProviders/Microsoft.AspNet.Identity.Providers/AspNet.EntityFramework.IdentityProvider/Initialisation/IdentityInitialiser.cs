dbd7f2718af1e177789f441ecc19a02bf5cd98fc
dg-mfb <danail.georgiev73@gmail.com>
2017-08-19 17:27:29 +0100

    identity provider refactoring

---
+++ AuthenticationProviders/Microsoft.AspNet.Identity.Providers/AspNet.EntityFramework.IdentityProvider/Initialisation/IdentityInitialiser.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks;
+using Kernel.DependancyResolver;
+using Shared.Initialisation;
+
+namespace AspNet.EntityFramework.IdentityProvider.Initialisation
+{
+    public class IdentityInitialiser : Initialiser
+    {
+        public override byte Order
+        {
+            get { return 0; }
+        }
+
+        protected override Task InitialiseInternal(IDependencyResolver dependencyResolver)
+        {
+            
+            return Task.CompletedTask;
+        }
+    }
+}
--- AuthenticationProviders/Microsoft.AspNet.Identity.Providers/AspNet.EntityFramework.IdentityProvider/Managers/ApplicationUserManager.cs
+++ AuthenticationProviders/Microsoft.AspNet.Identity.Providers/AspNet.EntityFramework.IdentityProvider/Managers/ApplicationUserManager.cs
@@ -1,38 +1,19 @@
 ﻿using AspNet.EntityFramework.IdentityProvider.Models;
 using Microsoft.AspNet.Identity;
-using Microsoft.AspNet.Identity.EntityFramework;
 
 namespace AspNet.EntityFramework.IdentityProvider.Managers
 {
     public class ApplicationUserManager : UserManager<ApplicationUser>
     {
-        public ApplicationUserManager(IUserStore<ApplicationUser> store)
+        public ApplicationUserManager(IUserStore<ApplicationUser> store, 
+            IUserTokenProvider<ApplicationUser, string> tokenProvider,
+            IIdentityValidator<string> passwordValidator,
+            IIdentityValidator<ApplicationUser> userValidator)
             : base(store)
         {
-        }
-
-        public static ApplicationUserManager Create(IUserTokenProvider<ApplicationUser, string> tokenProvider, ApplicationDbContext context)
-        {
-            var manager = new ApplicationUserManager(new UserStore<ApplicationUser>(context));
-            // Configure validation logic for usernames
-            manager.UserValidator = new UserValidator<ApplicationUser>(manager)
-            {
-                AllowOnlyAlphanumericUserNames = false,
-                RequireUniqueEmail = true
-            };
-            // Configure validation logic for passwords
-            manager.PasswordValidator = new PasswordValidator
-            {
-                RequiredLength = 6,
-                RequireNonLetterOrDigit = true,
-                RequireDigit = true,
-                RequireLowercase = true,
-                RequireUppercase = true,
-            };
-
-            manager.UserTokenProvider = tokenProvider;
-            
-            return manager;
+            base.UserTokenProvider = tokenProvider;
+            base.PasswordValidator = passwordValidator;
+            //base.UserValidator = userValidator;
         }
     }
 }
