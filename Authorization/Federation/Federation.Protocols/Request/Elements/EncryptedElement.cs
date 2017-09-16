d903ff7bf9b27bae70f0c10b76ef758ab070f5bb
Nadim Mansur <nadim.mansur@live.com>
2017-09-16 10:35:38 +0100

    Added Elements & Attributes

--- Authorization/Federation/Federation.Protocols/Request/Elements/EncryptedElement.cs
+++ Authorization/Federation/Federation.Protocols/Request/Elements/EncryptedElement.cs
@@ -1,9 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Security.Cryptography.Xml;
-using System.Text;
-using System.Threading.Tasks;
 using System.Xml.Serialization;
 
 namespace Federation.Protocols.Request.Elements
