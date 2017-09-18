5167828c63fdcac49ab3bf15c02c5f973869adde
dg-mfb <danail.georgiev73@gmail.com>
2017-09-18 11:39:36 +0100

    clean up

--- Authorization/Federation/JsonMetadataContextProvider/MetadataContextBuilder.cs
+++ Authorization/Federation/JsonMetadataContextProvider/MetadataContextBuilder.cs
@@ -4,11 +4,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Kernel.Federation.MetaData.Configuration;
+using Kernel.Serialisation;
 
 namespace JsonMetadataContextProvider
 {
     internal class MetadataContextBuilder : IMetadataContextBuilder
     {
+        private readonly ISerializer serialiser;
+
+        public MetadataContextBuilder(ISerializer serialiser)
+        {
+
+        }
         public MetadataContext BuildContext()
         {
             throw new NotImplementedException();
